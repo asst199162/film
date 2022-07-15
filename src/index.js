1c33baeaf408c3ddc4101e22d6809e53737cbda1
Konstantin Ligus <kostya1989com@gmail.com>
2022-07-15 19:53:40 +0300

    add js, html, scss partials

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import * as references from './js/references';
+import * as onSubmitClick from './js/onSubmitClick';
+import * as onPageNumerClick from './js/onPageNumerClick';
+import * as onCardClick from './js/onCardClick';
+import * as onWatchedAndQueueClick from './js/onWatchedAndQueueClick';
---
+++ src/js/onSubmitClick.js
@@ -0,0 +1,3 @@
+import * as searchItems from './searchItems';
+import * as createGallery from './createGallery';
+import * as numeration from './createNumeration';
