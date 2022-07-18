3645c18bf576652ab3095321804b7fa6eea22c53
Konstantin Ligus <kostya1989com@gmail.com>
2022-07-18 19:22:45 +0300

    edit js and html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import * as references from './js/references';
+import * as searchItems from './js/searchItems';
+import * as createGallery from './js/createGallery';
+import * as numeration from './js/createNumeration';
 import * as onSubmitClick from './js/onSubmitClick';
 import * as onPageNumerClick from './js/onPageNumerClick';
 import * as onCardClick from './js/onCardClick';
 import * as onWatchedAndQueueClick from './js/onWatchedAndQueueClick';
+import * as onAddToWatchedAndAddToQueueClick from './js/onAddToWatchedAndAddToQueueClick';
--- src/js/onSubmitClick.js
+++
@@ -1,3 +0,0 @@
-import * as searchItems from './searchItems';
-import * as createGallery from './createGallery';
-import * as numeration from './createNumeration';
