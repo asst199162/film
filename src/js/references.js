e39c372ccbbcf049391cdf02319006990280dc55
Konstantin Ligus <kostya1989com@gmail.com>
2022-07-19 17:26:07 +0300

    edit refs for js

---
+++ src/js/references.js
@@ -0,0 +1,7 @@
+const input = document.querySelector('[data-action="input"]');
+const gallery = document.querySelector('[data-gllery]');
+const pagination = document.querySelector('[data-pagination]');
+const modal = document.querySelector('[data-modal]');
+const headerMyLibButtons = document.querySelector(
+  '[data-header-my-lib-buttons]'
+);
