872ad0211a05a20ff4d33b71f78e38c1b3cabee9
Konstantin Ligus <kostya1989com@gmail.com>
2022-07-24 15:52:41 +0300

    add modal functionality

--- src/js/modalOpenClose.js
+++ src/js/modalOpenClose.js
@@ -16,33 +16,38 @@ refs.modalOpenClose.addEventListener('click', onModalOpen);
 
 export function onModalOpen(e) {
   e.preventDefault();
-  // if (e.target.parentNode.classList.contains('.gallery__item')) { return; }
   const card = e.target.parentNode;
   const id = card.getAttribute('id');
-  // try {
-  //   const response = await getMovieApi.fetchPopularMovie();
-  // let movie = response.results.find(movie => movie.id === Number(id));
-  movie = JSON.parse(localStorage.getItem("searchedMovies")).find(movie => movie.id === Number(id));
+
+  movie = JSON.parse(localStorage.getItem('searchedMovies')).find(
+    movie => movie.id === Number(id)
+  );
   renderMovieCard(movie);
   const btnRefs = {
     addQueue: document.querySelector('.modal-button-queue'),
     addWatched: document.querySelector('.modal-button-watched'),
-  }
+  };
   btnRefs.addQueue.addEventListener('click', onQueueBtnClick);
   btnRefs.addWatched.addEventListener('click', onBtnWatchedClick);
-  // } catch (error) {
-  //     console.log(error.message);
-  //   }
+
   refs.backdrop.classList.toggle('is-hidden');
   refs.closeBtn.addEventListener('click', onModalClose);
   refs.backdrop.addEventListener('click', onBackdropClose);
   window.addEventListener('keydown', closeOnKeydown);
 
-  if (JSON.parse(localStorage.getItem("watchedCard"))?.find(movie => movie.id === Number(id))) {
+  if (
+    JSON.parse(localStorage.getItem('watchedCard'))?.find(
+      movie => movie.id === Number(id)
+    )
+  ) {
     btnRefs.addWatched.textContent = 'REMOVE FROM WATCHED';
     btnRefs.addWatched.classList.add('watched');
   }
-  if (JSON.parse(localStorage.getItem("queuedCard"))?.find(movie => movie.id === Number(id))) {
+  if (
+    JSON.parse(localStorage.getItem('queuedCard'))?.find(
+      movie => movie.id === Number(id)
+    )
+  ) {
     btnRefs.addQueue.textContent = 'REMOVE FROM QUEUED';
     btnRefs.addQueue.classList.add('queued');
   }
@@ -70,22 +75,23 @@ function renderMovieCard(movie) {
 }
 
 function onBtnWatchedClick(e) {
-  console.log(movie);
-  // const watchedMovie = [];
   if (!e.target.classList.contains('watched')) {
-    if (JSON.parse(localStorage.getItem("watchedCard"))) { watchedMovie = JSON.parse(localStorage.getItem("watchedCard")) };
+    if (JSON.parse(localStorage.getItem('watchedCard'))) {
+      watchedMovie = JSON.parse(localStorage.getItem('watchedCard'));
+    }
     watchedMovie.push(movie);
-    localStorage.setItem("watchedCard", JSON.stringify(watchedMovie));
+    localStorage.setItem('watchedCard', JSON.stringify(watchedMovie));
     e.target.classList.add('watched');
     e.target.textContent = 'REMOVE FROM WATCHED';
     return;
-  } else
-    /* (e.target.classList.contains('watched')) */ {
-    watchedMovie = JSON.parse(localStorage.getItem("watchedCard"));
-    const filterWatchedMovie = watchedMovie.filter((item) => Number(item.id) !== Number(movie.id));
-    console.log(filterWatchedMovie);
+  } else {
+    /* (e.target.classList.contains('watched')) */
+    watchedMovie = JSON.parse(localStorage.getItem('watchedCard'));
+    const filterWatchedMovie = watchedMovie.filter(
+      item => Number(item.id) !== Number(movie.id)
+    );
     watchedMovie = [...filterWatchedMovie];
-    localStorage.setItem("watchedCard", JSON.stringify(watchedMovie));
+    localStorage.setItem('watchedCard', JSON.stringify(watchedMovie));
     e.target.classList.remove('watched');
     e.target.textContent = 'ADD TO WATCHED';
   }
@@ -93,18 +99,22 @@ function onBtnWatchedClick(e) {
 
 function onQueueBtnClick(e) {
   if (!e.target.classList.contains('queued')) {
-    if (JSON.parse(localStorage.getItem("queuedCard"))) { watchedMovie = JSON.parse(localStorage.getItem("queuedCard")) };
+    if (JSON.parse(localStorage.getItem('queuedCard'))) {
+      watchedMovie = JSON.parse(localStorage.getItem('queuedCard'));
+    }
     queuedMovie.push(movie);
-    localStorage.setItem("queuedCard", JSON.stringify(queuedMovie));
+    localStorage.setItem('queuedCard', JSON.stringify(queuedMovie));
     e.target.classList.add('queued');
     e.target.textContent = 'REMOVE FROM QUEUED';
     return;
-  } else
-    /* (e.target.classList.contains('watched')) */ {
-    queuedMovie = JSON.parse(localStorage.getItem("queuedCard"));
-    const filterQueuedMovie = queuedMovie.filter((item) => Number(item.id) !== Number(movie.id));
+  } else {
+    /* (e.target.classList.contains('watched')) */
+    queuedMovie = JSON.parse(localStorage.getItem('queuedCard'));
+    const filterQueuedMovie = queuedMovie.filter(
+      item => Number(item.id) !== Number(movie.id)
+    );
     queuedMovie = [...filterQueuedMovie];
-    localStorage.setItem("queuedCard", JSON.stringify(queuedMovie));
+    localStorage.setItem('queuedCard', JSON.stringify(queuedMovie));
     e.target.classList.remove('queued');
     e.target.textContent = 'ADD TO QUEUED';
   }
